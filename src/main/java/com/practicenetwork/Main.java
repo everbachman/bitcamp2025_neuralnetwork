b44a899df05431a59b7cacb5fb0d82cddaef21c1
Evelyn Bachman <eva.r.bachman@gmail.com>
2025-04-14 09:02:10 -0400

    first update

--- src/main/java/com/practicenetwork/Main.java
+++ src/main/java/com/practicenetwork/Main.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 
+
 public class Main {
     public static void main(String[] args) {
         if (true) {
@@ -44,57 +45,52 @@ public class Main {
                 network.train(inputs2, outputs2);
             }
 
-        Scanner scanner = new Scanner(System.in);
+            Scanner scanner = new Scanner(System.in);
 
-        int cont = 1;
+            int cont = 1;
 
-        while (cont == 1) {
-            System.out.println("Enter the first bit (1 or 0)");
-            double bit1 = scanner.nextInt();
-    
-            System.out.println("Enter the second bit (1 or 0)");
-            int bit2 = scanner.nextInt();
-    
-            double[] choosenInput = new double[2];
-            choosenInput[0] = bit1;
-            choosenInput[1] = bit2;
-            output = network.run(choosenInput);
-            if (output[0] > 0.5) output[0] = 1; else output[0] = 0;
-            if (output[1] > 0.5) output[1] = 1; else output[1] = 0;
-    
-            System.out.println("\nYour output for (" + (int) bit1 + " " + (int) bit2 + ") is: " + (int) output[0] + " " + (int) output[1]);
-            System.out.println("Do you want to try again? (1==yes, 0==no)");
-            cont = scanner.nextInt();
-        }
-        scanner.close();
-
-        if (false) {
-            output = network.run(input);
-            System.out.println(Arrays.toString(output));
-            double[] secondInput = {1, 0};
-            output = network.run(secondInput);
-            System.out.println(Arrays.toString(output));
-
-
-            double[] i3 = {0, 0};
-            output = network.run(i3);
-            System.out.println(Arrays.toString(output));
-            double[] i4 = {1, 1};
-            output = network.run(i4);
-            System.out.println(Arrays.toString(output));
-        }
-
-        } else {
-            Network001 network = new Network001(1, 2, 2, 4, 0.1);
-            double[][] inputs = {{0}, {1}, {2}, {0}, {1}, {2}, {0}, {1}, {2}, {0}, {1}, {2}};
-            double[][] outputs = {{0, 0}, {0, 1}, {1, 0}, {0, 0}, {0, 1}, {1, 0}, {0, 0}, {0, 1}, {1, 0}, {0, 0}, {0, 1}, {1, 0}};
-            for (int j = 0; j < 1000000; j++) {
-                network.train(inputs, outputs);
+            while (cont == 1) {
+                System.out.println("\nEnter the first bit (1 or 0)");
+                double bit1 = scanner.nextInt();
+        
+                System.out.println("Enter the second bit (1 or 0)");
+                int bit2 = scanner.nextInt();
+        
+                double[] choosenInput = new double[2];
+                choosenInput[0] = bit1;
+                choosenInput[1] = bit2;
+                output = network.run(choosenInput);
+                if (output[0] > 0.5) output[0] = 1; else output[0] = 0;
+                if (output[1] > 0.5) output[1] = 1; else output[1] = 0;
+        
+                System.out.println("\nYour output for (" + (int) bit1 + " " + (int) bit2 + ") is: " + (int) output[0] + " " + (int) output[1]);
+                System.out.println("Do you want to try again? (1==yes, 0==no)");
+                cont = scanner.nextInt();
+                System.out.print("\n");
             }
-            for (int i = 0; i < 3; i++) {
-                double[] output = network.run(inputs[i]);
+            
+
+
+            // previous testing code
+            
+            System.out.println("Do you want to see data? (1==yes, 0==no)");
+            cont = scanner.nextInt();
+            if (cont == 1) {
+                output = network.run(input);
+                System.out.println(Arrays.toString(output));
+                double[] secondInput = {1, 0};
+                output = network.run(secondInput);
+                System.out.println(Arrays.toString(output));
+
+
+                double[] i3 = {0, 0};
+                output = network.run(i3);
+                System.out.println(Arrays.toString(output));
+                double[] i4 = {1, 1};
+                output = network.run(i4);
                 System.out.println(Arrays.toString(output));
             }
+            scanner.close();
         }
 
     }
